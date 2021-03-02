6f8c3b0ae77de0e0ec5eb6c8bb114e2a694cb2ac
Anton <tohanshuklin@gmail.com>
2021-03-02 18:06:28 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,88 @@
 import './App.scss';
 import AsideMenu from './components/AsideMenu/AsideMenu.js'
+import Products from './components/Products/Products.js'
 
 function App() {
+  let autos = [{
+    img: "https://pbs.twimg.com/profile_images/713511184910139392/_hAw3t46_400x400.jpg",
+    title: "Tesla Model X",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: "$132000"
+  },
+  {
+    img: "https://pbs.twimg.com/profile_images/713511184910139392/_hAw3t46_400x400.jpg",
+    title: "Tesla Model X",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: "$132000"
+  },
+  {
+    img: "https://pbs.twimg.com/profile_images/713511184910139392/_hAw3t46_400x400.jpg",
+    title: "Tesla Model X",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: "$132000"
+  },
+  {
+    img: "https://pbs.twimg.com/profile_images/713511184910139392/_hAw3t46_400x400.jpg",
+    title: "Tesla Model X",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: "$132000"
+  },{
+    img: "https://pbs.twimg.com/profile_images/713511184910139392/_hAw3t46_400x400.jpg",
+    title: "Tesla Model X",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: "$132000"
+  },
+  {
+    img: "https://pbs.twimg.com/profile_images/713511184910139392/_hAw3t46_400x400.jpg",
+    title: "Tesla Model X",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: "$132000"
+  },
+  {
+    img: "https://pbs.twimg.com/profile_images/713511184910139392/_hAw3t46_400x400.jpg",
+    title: "Tesla Model X",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: "$132000"
+  },
+  {
+    img: "https://pbs.twimg.com/profile_images/713511184910139392/_hAw3t46_400x400.jpg",
+    title: "Tesla Model X",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: "$132000"
+  },
+  {
+    img: "https://pbs.twimg.com/profile_images/713511184910139392/_hAw3t46_400x400.jpg",
+    title: "Tesla Model X",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: "$132000"
+  },
+  {
+    img: "https://pbs.twimg.com/profile_images/713511184910139392/_hAw3t46_400x400.jpg",
+    title: "Tesla Model X",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: "$132000"
+  },
+  {
+    img: "https://pbs.twimg.com/profile_images/713511184910139392/_hAw3t46_400x400.jpg",
+    title: "Tesla Model X",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: "$132000"
+  },
+  {
+    img: "https://pbs.twimg.com/profile_images/713511184910139392/_hAw3t46_400x400.jpg",
+    title: "Tesla Model X",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: "$132000"
+  }]
+  let autoCard = autos.map((auto :{img: string; title: string; info: string; price: string;}, index :number) => {
+    return <Products product={auto} key={index}/>
+  })
   return (
     <div className="App">
       <AsideMenu/>
+      <div className="Products-group">
+        {autoCard}
+      </div>
     </div>
   );
 }
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,13 +1,15 @@
 import './Products.scss';
 
 function Products(props) {
+  let {product} = props
   return (
     <div className="Products">
         <div className="Products__image">
             <img src={props.img} alt=""/>
         </div>
-        <div className="Products__title">{props.title}</div>
-        <div className="Products__info">{props.info}</div>
+        <div className="Products__title">{product.title}</div>
+        <div className="Products__info">{product.info}</div>
+        <div className="Products__price">{product.price}</div>
         <button className="Products__btn">Подробнее</button>
     </div>
   );
